776fe00132b674726f945e9372b18368d69242a9
calwe <spikywebb@gmail.com>
2024-02-26 23:24:30 +0000

    added vim-nix, plus theme

--- config/default.nix
+++ config/default.nix
@@ -1,6 +1,20 @@
+{ pkgs, ... }:
 {
   # Import all your configuration modules here
   imports = [
     ./bufferline.nix
   ];
+
+  colorschemes.catppuccin.enable = true;
+
+  plugins = {
+    lualine.enable = true;
+    neo-tree.enable = true;
+    nix.enable = true;
+    lsp = {
+      enable = true;
+      servers = {
+      };
+    };
+  };
 }
