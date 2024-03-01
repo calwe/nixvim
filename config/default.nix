db203e74a684d787602d12fba37ebbb298fcb047
calwe <spikywebb@gmail.com>
2024-03-01 14:16:41 +0000

    add lsp

--- config/default.nix
+++ config/default.nix
@@ -24,9 +24,43 @@
       lsp = {
         enable = true;
         servers = {
+          rust-analyzer.enable = true;
+        };
+      };
+
+      nvim-cmp = {
+        enable = true;
+        autoEnableSources = true;
+        sources = [
+          { name = "nvim_lsp"; }
+          { name = "path"; }
+          { name = "nuffer"; }
+        ];
+
+        mapping = {
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<Tab>" = {
+            action = ''
+              function(fallback)
+                if cmp.visible() then
+                  cmp.select_next_item()
+                elseif luasnip.expandable() then
+                  luasnip.expand()
+                elseif luasnip.expand_or_jumpable() then
+                  luasnip.expand_or_jump()
+                elseif check_backspace() then
+                  fallback()
+                else
+                  fallback()
+                end
+              end
+            '';
+            modes = [ "i" "s" ];
+          };
         };
       };
     };
+
 
     globals.mapleader = " ";
     keymaps = [
